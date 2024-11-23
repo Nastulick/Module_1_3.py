855ed19354984dd76b93aef2bc1859a4f9c42c7a
Nastulick <nastya9031976011@gmail.com>
2024-11-23 19:22:18 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,5 @@
+completed_homeworks=12
+spent_hours=1.5
+course_name='Python'
+time_for_one_task=0.125
+print('курс',course_name,'всего задач:',completed_homeworks,'затрачено часов',spent_hours,'среднее время выполнения',time_for_one_task)
